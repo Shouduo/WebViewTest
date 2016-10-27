4d1d01106678444fc7189b1d7452f01a026d36af
Shouduo <handsomesaywhynot@gmail.com>
2016-10-27 22:05:56 +0800

    Use shouldOverrideUrlLoading(WebView, WebResourceRequest) instead. And change the minSdkVersion to 21.

--- app/src/main/java/com/shouduo/webviewtest/MainActivity.java
+++ app/src/main/java/com/shouduo/webviewtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.shouduo.webviewtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -16,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
         mWebView = (WebView) findViewById(R.id.web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                view.loadUrl(url);
+//                return true;
+//            }
+
+
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return super.shouldOverrideUrlLoading(view, request);
             }
         });
         mWebView.loadUrl("https://m.baidu.com");
