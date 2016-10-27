617df418c62502401511980184b57902cc69d6cb
Shouduo <handsomesaywhynot@gmail.com>
2016-10-27 21:23:56 +0800

    First commit

---
+++ app/src/main/java/com/shouduo/webviewtest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.shouduo.webviewtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mWebView.loadUrl("https://m.baidu.com");
+    }
+}
